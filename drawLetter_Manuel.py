761e709d7bd40d6fd20a87a67adc68383ab271da
thatbob9001 <spis18bs@its-cseb230-19.ucsd.edu>
2018-08-13 14:16:18 -0700

    MU Used loops for drawM()

--- drawLetter_Manuel.py
+++ drawLetter_Manuel.py
@@ -1,30 +1,49 @@
+# Makes sure that 
 import turtle
 
+
 def drawM(theTurtle, xStart, yStart):
+''' Draws letter M then moves turtle forward '''
+    # Keeps track of initial position of the turtle
     curX = xStart
     curY = yStart
-    theTurtle.setheading(90)
-    theTurtle.forward(100)
 
-    curX = xStart + 50
-    curY = yStart + 50
-    theTurtle.goto(curX, curY)
+    displacementLists = [(0, 100), (50, 50), (100, 100), (100, 0)]
 
-    curX = xStart + 100
-    curY = yStart + 100
-    theTurtle.goto(curX, curY)
+    # Used list of displacements from the original position, for sleeker/more readable code
+    for coordinate in displacementLists:
+        curX = xStart + coordinate[0]
+        curY = yStart + coordinate[1]
+        theTurtle.goto(curX, curY)
 
-    curX = xStart + 100
-    curY = yStart
-    theTurtle.goto(curX, curY)
 
+""" OLD VERSION OF PROGRAM
+    Used repetitive code, replaced with for loop and list of displacements """
+#    theTurtle.setheading(90)
+#    theTurtle.forward(100)
+#
+#    curX = xStart + 50
+#    curY = yStart + 50
+#    theTurtle.goto(curX, curY)
+#
+#    curX = xStart + 100
+#    curY = yStart + 100
+#    theTurtle.goto(curX, curY)
+#
+#    curX = xStart + 100
+#    curY = yStart
+#    theTurtle.goto(curX, curY)
+
+    # Gets turtle ready for more text input
     theTurtle.pu()
     curX = xStart + 110
     theTurtle.goto(curX, yStart)
     theTurtle.pd()
 
+# Creates turtle object
 myTurtle = turtle.Turtle()
 
+# Calls function, and passes current coordinates
 drawM(myTurtle, myTurtle.xcor(), myTurtle.ycor())
 drawM(myTurtle, myTurtle.xcor(), myTurtle.ycor())
 drawM(myTurtle, myTurtle.xcor(), myTurtle.ycor())
