da5ff75caa7db3ffe76bbc6029dcbbad51f23f6a
DarioAburto <42155110+DarioAburto@users.noreply.github.com>
2018-08-13 14:23:25 -0700

    Update lab03Warmup_Dario.py

--- lab03Warmup_Dario.py
+++ lab03Warmup_Dario.py
@@ -1,1 +1,14 @@
-#Turtle will draw the letter D
+def drawPicture(theTurtle):
+    ''' Draw a simple picture using a turtle '''
+    theTurtle.forward(100)
+    theTurtle.left(90)
+    theTurtle.forward(100)
+    theTurtle.left(90)
+    theTurtle.forward(100)
+    theTurtle.left(90)
+    theTurtle.forward(100)
+    theTurtle.left(90)    
+
+myTurtle = turtle.Turtle()  # Create a new Turtle object
+drawPicture(myTurtle)   # make the new Turtle draw the shape
+
